ef19075d4a5fa5f0d6959c5724f4f7c2e7b8b0f9
ttjiang2 <ttjiang2@illinois.edu>
2014-02-12 18:31:07 -0600

    file upload + display working, deleting extraneous files

---
+++ pls.py
@@ -0,0 +1,55 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+
+from gi.repository import GLib
+
+UPLOAD_FOLDER = GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_DOWNLOAD) + '/pls_respond_uploaded'
+
+# Initialize
+app = Flask(__name__)
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    return render_template('landing.html')
+
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the the uploaded file
+    file = request.files['hidden-upload']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file from the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+
+@app.route(UPLOAD_FOLDER + '/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'oopsie 404'
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
